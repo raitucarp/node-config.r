99c7dac4877c7004edee2c66538e8905d0a5cd03
Raitucarp <ribhararnus.pracutiar@gmail.com>
2013-11-14 15:13:35 +0700

    add path, refactor some code

--- index.js
+++ index.js
@@ -4,40 +4,93 @@ var fs   = require('fs'),
 var __parentDir = path.dirname(module.parent.filename);
 
 var configr = function () {
-    var configName = '/config.json',
-        data, x;
-    
+	this.$path = '';
+	this.init();
+};
+
+configr.prototype.init = function () {
+	this.data = {};
+	var configName = '/config.json',
+        data, _path, x;
+    if (this.$path !== '') {
+		_path = path.resolve(__parentDir, this.$path);
+    } else {
+		_path = __parentDir;
+    }
+      
     try {
-        data = require(__parentDir + configName);
+        data = require(_path + configName);
     } catch (e) {
         data = {};
     }
     for (x in data) {
         if (data.hasOwnProperty(x)) {
-            this[x] = data[x];
+            this.data[x] = data[x];
         }
     }
 };
 
 configr.prototype.load = function (name) {
-    this[name] = {};
-    var configName = '/' + name + '.json',
-        data, x;
-    try {
-        data = require(__parentDir + configName);
-    } catch (e) {
-        data = {};
+	var n, _path;
+	name = name.split(',');
+	if (this.$path !== '') {
+		_path = path.resolve(__parentDir, this.$path);
+    } else {
+		_path = __parentDir;
     }
-    for (x in data) {
-        if (data.hasOwnProperty(x)) {
-            this[name][x] = data[x];
-        }
-    }
-    return this;
+	for (n in name) {
+		if (name.hasOwnProperty(n)) {
+			var _name = name[n].replace(/^\s+|\s+$/g, '');
+			this.data[_name] = {};
+			var configName = '/config.' + _name + '.json',
+				data, x;
+			try {
+				data = require(_path + configName);
+			} catch (e) {
+				data = {};
+				delete this.data[_name];
+			}
+
+			for (x in data) {
+				if (data.hasOwnProperty(x)) {
+					this.data[_name][x] = data[x];
+				}
+			}
+		}
+	}
+	return this;
+};
+
+configr.prototype.get = function (name) {
+	if (typeof name === 'undefined') {
+		return this.data;
+	} else {
+		var n, select, parent;
+		name = name.split('.');
+		for (n in name) {
+			if (name.hasOwnProperty(n)) {
+				var _name = name[n];
+				if (typeof select === 'undefined') {
+					select = this.data[_name];
+				} else {
+					select = select[_name];
+				}
+			}
+		}
+		
+		select.length = _.size(select);
+		return select;
+	}
+};
+
+configr.prototype.path = function (_path) {
+	this.$path = _path;
+	this.init();
+	return this;
 };
 
 configr.prototype.where = function (condition) {
-    return _.where(this, condition);
+    return _.findWhere(this, condition);
 };
 
 module.exports = new configr;
